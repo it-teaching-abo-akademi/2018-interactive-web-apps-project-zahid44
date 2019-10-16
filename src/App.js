b9dfce56e083635d91006e6db43cbe106de9528d
zahid44 <zahid.choudhury@gmail.com>
2019-10-16 16:50:16 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import SPMS from './components/spms';
+
+function App() {
+  return (
+    <SPMS />
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NewPortfolio from './components/new_portfolio';
+import NewStock from './components/new_stock';
+import ViewGraph from './components/view_graph';
+
+// This is entry point of program. This file controls routing of program.
+const routing = (
+		<Router>
+			<Route exact path='/' component={App}/>	// The url '/' is equivalent to App component.
+      <Route path='/portfolio/add' component={NewPortfolio}/> // The url '/' is equivalent to NewPortfolio component. This component is one for adding new portfolio.
+      <Route path='/stock/add/:index' component={NewStock}/> // The url '/' is equivalent to App component. This component is one for adding new stock.
+			<Route path='/stock/viewgraph/:index' component={ViewGraph}/> // The url '/' is equivalent to App component. This component is one for viewing graphs.
+		</Router>
+	);
+ReactDOM.render(routing, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
