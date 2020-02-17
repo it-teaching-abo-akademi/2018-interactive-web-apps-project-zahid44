884c1669506e59f58ef320e44da6c1b650678198
dtruscan <dragos.truscan@abo.fi>
2020-02-17 17:35:39 +0200

    Updates

---
+++ precache-manifest.27d070852337f069d54651e80b3fe4eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0a09ff6d04b48636ce9a649ccdeae7",
+    "url": "/2018-interactive-web-apps-project-zahid44/index.html"
+  },
+  {
+    "revision": "f8b7cdcdf37cb3b794bf",
+    "url": "/2018-interactive-web-apps-project-zahid44/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "d8a68356334b43a40803",
+    "url": "/2018-interactive-web-apps-project-zahid44/static/css/main.bf80196a.chunk.css"
+  },
+  {
+    "revision": "f8b7cdcdf37cb3b794bf",
+    "url": "/2018-interactive-web-apps-project-zahid44/static/js/2.6dd95d11.chunk.js"
+  },
+  {
+    "revision": "d8a68356334b43a40803",
+    "url": "/2018-interactive-web-apps-project-zahid44/static/js/main.ff7d9d74.chunk.js"
+  },
+  {
+    "revision": "4d11aa3c0bc4ae54d476",
+    "url": "/2018-interactive-web-apps-project-zahid44/static/js/runtime-main.acc56639.js"
+  }
+]);
